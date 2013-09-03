44b818b0f9cc0d0bde1561fc49889277cb0777d6
Daniel Weil <danweil68@gmail.com>
2013-09-03 14:52:16 +0200

    new version of atdgen plugin

--- myatdgen.ml
+++ myatdgen.ml
@@ -1,17 +1,39 @@
 open Ocamlbuild_plugin
 
+let _ = 
+  Options.use_ocamlfind := true
+    
 let () = dispatch begin function
-    | After_rules ->
-      rule "atdgen: .atd -> _t.ml*, _j.ml*, _v.ml*"
-        ~prods:["%_t.ml";"%_t.mli";"%_j.ml";"%_j.mli";"%_v.ml";"%_v.mli";]
-        ~dep:"%.atd"   
-        (begin fun env build ->
-           let atdgen = "atdgen" in
-           Seq [          
-             Cmd (S [A atdgen; A "-t"; P (env "%.atd")]);
-             Cmd (S [A atdgen; A "-j"; A "-j-std"; P (env "%.atd")]);
-             Cmd (S [A atdgen; A "-v"; P (env "%.atd")]);
-           ]              
-         end);
-    | _ -> ()
-  end
+  | After_rules ->
+    let tag_atdgen env patterns = 
+      List.iter (fun p -> tag_file (env p) (Tags.elements (Tags.of_list ["package(atdgen)"]))) patterns
+    in
+
+    rule "atdgen: .atd -> _t.ml*"
+      ~prods:["%_t.ml";"%_t.mli"]
+      ~dep:"%.atd"
+      (begin fun env build ->
+        let atdgen = "atdgen" in
+        tag_atdgen env ["%_t.ml";"%_t.mli"];
+        Cmd (S [A atdgen; A "-t"; P (env "%.atd")]);
+       end) ;
+    rule "atdgen: .atd -> _j.ml*"
+      ~prods:["%_j.ml";"%_j.mli";]
+      ~dep:"%.atd"
+      (begin fun env build ->
+        let atdgen = "atdgen" in
+        tag_atdgen env ["%_j.ml"; "%_j.mli"];
+        Cmd (S [A atdgen; A "-j"; A "-j-std"; P (env "%.atd")]);
+       end) ;
+    rule "atdgen: .atd -> _v.ml*"
+      ~prods:["%_v.ml";"%_v.mli";]
+      ~dep:"%.atd"
+      (begin fun env build ->
+        let atdgen = "atdgen" in
+        tag_atdgen env ["%_v.ml";"%_v.mli";];
+        Cmd (S [A atdgen; A "-v"; P (env "%.atd")]);
+       end) ;
+    ()
+      
+  | _ -> ()
+end
